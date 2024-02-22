1e3f2fcff501da53e70d23b849668bc3e4d23238
Swierszczykowaty <loolok1232@gmail.com>
2024-02-22 17:02:44 +0100

    [ADD] - Hero animation circle, gradient bg

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-background ">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
           <Hero/>
           <About/>
 
-        </div>
       </div>
     </BrowserRouter>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import './Hero.css'
 const Hero = () => {
   return (
-    <>
-      <div>
-        Hero
+    <div className="relative z-0">
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+
+        </ul>
       </div>
-    </>
+      <div className="context w-full absolute top-1/2">
+        <h1 className="text-center text-text text-8xl">
+          Michał Świercz
+        </h1>
+        
+      </div>
+    </div>
   );
 };
 
