e4bce9f504b1bf5065c9229bb75d409a0f66f8cc
Swierszczykowaty <loolok1232@gmail.com>
2024-02-22 21:04:46 +0100

    [UPDATE] - hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,22 +5,11 @@ const Hero = () => {
     <div className="relative z-0">
       <div className="area">
         <ul className="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-
+        {[...Array(12)].map((_, index) => <li key={index}></li>)}
         </ul>
       </div>
       <div className="context w-full absolute top-1/2">
-        <h1 className="text-center text-text text-8xl">
+        <h1 className="text-center text-text text-8xl px-40 sm:px-10 sm:text-xl">
           Michał Świercz
         </h1>
         
