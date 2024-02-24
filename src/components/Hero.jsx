ce0757143c0b8714bef31bc2e859dd799b8745d1
Swierszczykowaty <loolok1232@gmail.com>
2024-02-24 22:31:08 +0100

    [ADD] - Animation Hero reval block [need fix 1px line]

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,19 +10,21 @@ const Hero = () => {
         </ul>
       </div>
 
-      <div className= {`${styles.padingX}  md:px-80 xl:20px px-6 mx-auto context w-full absolute top-[220px] flex flex-row gap-5`}>
+      <div className= {`${styles.paddingX} justify-center mx-auto context w-full absolute top-[220px] flex flex-row gap-5`}>
         <div className="flex flex-col items-center mt-5 justify-center">
-          <div className="w-3 h-3 bg-violet-600 rounded-full p-3 "></div>
+          <div className="w-3 h-3 bg-[#804dee] rounded-full p-3 "></div>
           <div className="w-1 sm:h-96 h-40 violet-gradient"></div>
         </div>
         <div>
           <h1 className= {`${styles.heroHeadText} text-text drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}>
-            Hi, I'm <span className="text-violet-600 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] ">
+            Hi, I'm <span className="text-[#EDE34C] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] ">
             Michał Świercz
             </span>
           </h1>
-          <p className={`${styles.heroSubText} text-text drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}>
+          <p className={`${styles.heroSubText} w-max text-text drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] block-effect`}style={{ '--td': '0.8s' }}>
+          <div className="block-reveal" style={{ '--bc': '#804dee', '--d': '.1s' }}>
             student, designer & creator 
+          </div>
           </p>
         </div>
       </div>
