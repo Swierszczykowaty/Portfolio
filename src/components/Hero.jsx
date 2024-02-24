65971c193a7362a60818dfc4f78d8e0eeaa40a95
Swierszczykowaty <loolok1232@gmail.com>
2024-02-24 19:16:42 +0100

    [ADD] - Hero update, font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Hero.css'
+import { styles } from "../styles";
 const Hero = () => {
   return (
     <div className="relative z-0">
@@ -8,11 +9,22 @@ const Hero = () => {
         {[...Array(12)].map((_, index) => <li key={index}></li>)}
         </ul>
       </div>
-      <div className="context w-full absolute top-1/2">
-        <h1 className="text-center text-text text-8xl px-40 sm:px-10 sm:text-xl">
-          Michał Świercz
-        </h1>
-        
+
+      <div className= {`${styles.padingX}  md:px-80 xl:20px px-6 mx-auto context w-full absolute top-[220px] flex flex-row gap-5`}>
+        <div className="flex flex-col items-center mt-5 justify-center">
+          <div className="w-3 h-3 bg-violet-600 rounded-full p-3 "></div>
+          <div className="w-1 sm:h-96 h-40 violet-gradient"></div>
+        </div>
+        <div>
+          <h1 className= {`${styles.heroHeadText} text-text drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}>
+            Hi, I'm <span className="text-violet-600 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] ">
+            Michał Świercz
+            </span>
+          </h1>
+          <p className={`${styles.heroSubText} text-text drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]`}>
+            student, designer & creator 
+          </p>
+        </div>
       </div>
     </div>
   );
