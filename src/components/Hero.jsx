21af40ed7b17531fc16e43bd0b04bc93ad0e4f85
Swierszczykowaty <loolok1232@gmail.com>
2024-05-05 14:52:47 +0200

    [ADD] company animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,15 @@ import { styles } from "../styles";
 import { FaLongArrowAltDown } from "react-icons/fa";
 import { fadeIn } from "../untils/motion";
 import { motion } from 'framer-motion';
+import { companies } from "../constans";
+
+const Companylogo = ({index,title,photo,marginLeft, size}) =>{
+  return(
+    <div className={`absolute h-full w-full  left-[${marginLeft}]`}>
+      <img src={photo} alt="logo_company" className={`h-${size} w-${size}`} />
+    </div>
+  );
+};
 
 const Hero = () => {
   
@@ -13,6 +22,9 @@ const Hero = () => {
         <ul className="circles">
         {[...Array(12)].map((_, index) => <li key={index}></li>)}
         </ul>
+        {companies.map((company,index) =>(
+          <Companylogo key={index} index={index} {...company}/>
+        ))}
       </div>
       <div className="flex flex-col">
         <div className= {`${styles.paddingX} justify-center mx-auto context w-full h-2/3 absolute top-1/3 flex flex-row gap-5`}>
--- src/constans/index.js
+++ src/constans/index.js
@@ -61,19 +61,32 @@ export const navLinks = [
     title: "Contact",
   },
 ];
-
-const company = [
+//in 0% - 75% //
+const companies = [
   {
     photo: javascript,
+    marginLeft: "10%",
+    size: "56",
   },
   {
     photo: javascript,
+    marginLeft: "20%",
+    size: "52",
   },
   {
     photo: javascript,
+    marginLeft: "30%",
+    size: "20",
   },
   {
     photo: javascript,
+    marginLeft: "60%",
+    size: "96",
+  },
+  {
+    photo: javascript,
+    marginLeft: "75%",
+    size: "40",
   },
 ];
 
@@ -310,4 +323,4 @@ export const footicons = [
   },
 ];
 
-  export { services, company, web_pages, pandas, okz, photography};
+  export { services, companies, web_pages, pandas, okz, photography};
