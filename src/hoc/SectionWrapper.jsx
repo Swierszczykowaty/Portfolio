23ac842aaeb88829c3cf9085d69866be7bc3df0d
Swierszczykowaty <loolok1232@gmail.com>
2024-10-07 23:28:57 +0200

    [ADD] - update wrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -10,7 +10,6 @@ function HOC(){
         whileInView="show"
         viewport={{once:true, amount:0.25}}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
-            
             <span className="hash-span" id={idName}>
                 &nbsp;
             </span>
